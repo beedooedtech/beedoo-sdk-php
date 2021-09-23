ea9c36e3c866a1217393a5830d68289ec1efa15d
Igor Santana <igor.santana@beedoo.com.br>
2021-09-23 15:58:03 -0300

    Criado classe de Test para testar o funcionamento do PHPUnit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BeedooTest;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function shouldReturnTrue()
+    {
+        $this->assertTrue(true);
+    } 
+}
