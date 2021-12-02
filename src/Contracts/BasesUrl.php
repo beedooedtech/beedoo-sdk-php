c3266e4d4eccda935c7cfa1b943496350079c356
Igor Santana <igor.santana@beedoo.com.br>
2021-12-02 15:28:16 -0300

    Correção na rota padrão da API localhost

--- src/Contracts/BasesUrl.php
+++ src/Contracts/BasesUrl.php
@@ -5,7 +5,7 @@ namespace Beedoo\Contracts;
 interface BasesUrl {
 
     const BASES_URL = [
-        'dev' => 'https://localhost:4020/',
+        'dev' => 'http://localhost:4020',
         'hml' => 'https://hml-api.beedoo.io:4020/',
         'production' => 'https://api.beedoo.io/',
     ];
